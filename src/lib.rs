653d15e3774d84d15f73483cb3e6d51315e5ee06
Jay Kickliter <jay@kickliter.com>
2017-02-28 09:07:20 -0800

    Add get method

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub trait KV: Sized + PartialEq {}
 /// A node in a binary search tree
 #[derive(Debug, PartialEq, Clone)]
 pub struct Node<K, V>
-    where K: Ord + PartialEq
+    where K: Ord
 {
     /// This node's key
     key: K,
@@ -21,7 +21,7 @@ pub struct Node<K, V>
 }
 
 impl<K, V> Node<K, V>
-    where K: Ord + PartialEq
+    where K: Ord
 {
     pub fn new(k: K, v: V) -> Node<K, V> {
         Node {
@@ -35,7 +35,7 @@ impl<K, V> Node<K, V>
     pub fn insert(&mut self, node: Node<K, V>) {
         match self.key.cmp(&node.key) {
             Ordering::Less => {
-                match self.right {
+                match self.left {
                     None => self.right = Some(Box::new(node)),
                     Some(ref mut right) => right.insert(node),
                 }
@@ -50,6 +50,14 @@ impl<K, V> Node<K, V>
         }
     }
 
+    pub fn get(&self, key: &K) -> Option<&V> {
+        match self.key.cmp(key) {
+            Ordering::Equal => Some(&self.value),
+            Ordering::Less => self.right.as_ref().map_or(None, |node| node.get(key)),
+            Ordering::Greater => self.left.as_ref().map_or(None, |node| node.get(key)),
+        }
+    }
+
     pub fn children(&self) -> usize {
         self.left.as_ref().map_or(0, |node| 1 + node.children()) +
         self.right.as_ref().map_or(0, |node| 1 + node.children())
@@ -60,6 +68,7 @@ impl<K, V> Node<K, V>
 #[cfg(test)]
 mod tests {
     use ::Node;
+
     #[test]
     fn node_eq_pass() {
         let node_a: Node<String, String> = Node::new("cat".into(), "meow".into());
@@ -107,5 +116,18 @@ mod tests {
         };
         assert_eq!(node_root, node_root_1);
         assert_eq!(node_root.children(), 2);
+    }
+
+    #[test]
+    fn node_test_get_pass() {
+        let mut node_root = Node::new(1, 'b');
+        let node_left = Node::new(0, 'a');
+        let node_right = Node::new(2, 'c');
+        node_root.insert(node_right);
+        node_root.insert(node_left);
+
+        assert_eq!(node_root.get(&0), Some(&'a'));
+        assert_eq!(node_root.get(&1), Some(&'b'));
+        assert_eq!(node_root.get(&2), Some(&'c'));
     }
 }
